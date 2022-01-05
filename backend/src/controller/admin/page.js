6dba2aaa6989bf463463af6728a4fe2982a60aaa
Aroushi Sharma <sharma.aroushi@gmail.com>
2022-01-05 17:22:37 +0530

    api changes

--- backend/src/controller/admin/page.js
+++ backend/src/controller/admin/page.js
@@ -5,13 +5,13 @@ exports.createPage = (req, res) => {
   const { banners, products } = req.files;
   if (banners && banners.length > 0) {
     req.body.banners = banners.map((banner, index) => ({
-      img: `http://localhost:2000/public/${banner.filename}`,
+      img: `/public/${banner.filename}`,
       navigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`,
     }));
   }
   if (products && products.length > 0) {
     req.body.products = products.map((product, index) => ({
-      img: `http://localhost:2000/public/${product.filename}`,
+      img: `/public/${product.filename}`,
       navigateTo: `/productClicked?categoryId=${req.body.category}&type=${req.body.type}`,
     }));
   }
